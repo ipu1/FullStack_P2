d91f6b64066e00bc479aa83e89f903b0790b21d3
ipu1 <ilmari.muhonen@gmail.com>
2024-05-03 20:36:41 +0300

    update cors*

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors()); // Enable CORS for all routes
+app.use(cors('*')); // Enable CORS for all routes
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://myAtlas-001:1234@cluster0.uhslgbg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
@@ -22,6 +22,14 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
+
+// Middleware to set Content-Type header for CSS files
+app.use((req, res, next) => {
+    if (req.url.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    }
+    next();
+  });
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
